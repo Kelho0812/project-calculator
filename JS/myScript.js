50d05bc0f13bec1d2295e2cfa7cfb1db2fa6916c
Kelho <Kelho0812@hotmail.com>
2022-03-07 20:03:55 +0000

    Basic Math functions added

---
+++ JS/myScript.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+    let result = x + y;
+    return result;
+}
+
+function subtract(x, y) {
+    let result = x - y;
+    return result;
+}
+
+function multiply(x, y) {
+    let result = x * y;
+    return result;
+}
+
+function divide(x, y) {
+    let result = x / y;
+    return result;
+}
