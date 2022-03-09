916f7070b72f98e5718f615f678ad27ed988a2a6
Kelho <Kelho0812@hotmail.com>
2022-03-09 15:17:32 +0000

    Added Operate Function

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,19 +1,30 @@
-function add(x, y) {
-    let result = x + y;
-    return result;
+function operate(operator, num1, num2) {
+    function add(num1, num2) {
+        let result = num1 + num2;
+        return result;
+    }
+    function subtract(x, y) {
+        let result = x - y;
+        return result;
+    }
+    function multiply(x, y) {
+        let result = x * y;
+        return result;
+    }
+    function divide(x, y) {
+        let result = x / y;
+        return result;
+    }
+
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    }
 }
 
-function subtract(x, y) {
-    let result = x - y;
-    return result;
-}
-
-function multiply(x, y) {
-    let result = x * y;
-    return result;
-}
-
-function divide(x, y) {
-    let result = x / y;
-    return result;
-}
+console.log(operate("/", 44, 22));
